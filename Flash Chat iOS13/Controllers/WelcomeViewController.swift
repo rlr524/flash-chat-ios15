48247d4be6111ca515bff6cc941b548ddca2655c
Rob Ranf <rob@emiyaconsulting.com>
2022-02-17 12:16:08 -0800

    adding animation to title text

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,9 +14,16 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        // Set the title lable to a blank string then loop through the label, char by char, as an animation
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for char in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.2 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(char)
+            }
+            charIndex += 1
+        }
     }
-    
-
 }
